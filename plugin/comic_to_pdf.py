34c1632b82e6d3c6862f00adad24e2fc74ab8de0
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-06-29 23:00:13 +0530

    Update comic_to_pdf.py

--- plugin/comic_to_pdf.py
+++ plugin/comic_to_pdf.py
@@ -22,7 +22,7 @@ def comic2Pdf(client, callback_query):
         os.mkdir(f"Download")
     else:
         os.mkdir(f"Download")
-    comiclink = f"https://www.comicextra.com/{comic}/chapter-{chapNumber}"
+    comiclink = f"https://www.comicextra.com/{comic}/chapter-{chapNumber}/full"
     response = requests.get(comiclink)
     plainText = response.text
     soup = BeautifulSoup(plainText, "html.parser")
@@ -31,14 +31,17 @@ def comic2Pdf(client, callback_query):
     for url in source_url:
         comicUrlsSplit.append(url['src'])
         print(f"k {mangaUrlsSplit}")
+        total = len(mangaUrlsSplit)
         i = 1
-        for urls in comicUrlsSplit:    
+        for urls in comicUrlsSplit:   
+            callback_query.edit_message_text(f"""DOWNLOADING NOW\nProgress: `{round((int(i)/int(total)*100))}%`""", parse_mode="markdown")
             print(f"n {urls}")       
             response = requests.get(urls)
             with open(f"Download/{i}.png", "wb") as pic:
                 pic.write(response.content)
                 pic.close()
             i += 1       
+        callback_query.edit_message_text("""Uploading Now""", parse_mode="markdown")
         file_paths = []
         for root, directories, files in os.walk(f"Download"):
             for filename in files:
