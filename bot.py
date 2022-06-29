b69bf5a9f2e66011bf169b04d5884494346a34f0
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-06-29 21:11:49 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+
+from pyrogram import *
+from pyrogram.filters import regex, text_filter
+from pyrogram.methods.utilities.start import Start
+import config
+import logging
+from pyrogram.handlers import *
+from plugin.comic_search import comicsearch
+from plugin.comic_index import comicindex
+from plugin.comic_to_pdf import comic2Pdf
+from plugin.start import start
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+
+bot = Client(
+    "comic",
+    api_id=config.api_id,
+    api_hash=config.api_hash,
+    bot_token=config.bot_token
+)
+
+
+
+def main():
+    bot.add_handler(MessageHandler(comicsearch, filters.regex(r'comic')), group=1)
+    bot.add_handler(CallbackQueryHandler(comicindex, filters.regex('but_*')), group=2)
+    bot.add_handler(CallbackQueryHandler(comic2Pdf, filters.regex('pdfr_*')), group=6)
+    bot.add_handler(MessageHandler(start , filters.regex(r'start')), group=13)
+
+
+if __name__ == '__main__':
+    bot.run(main())
