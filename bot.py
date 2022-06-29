8b2cecd43a6312e15ab7ea2344c9543c54628648
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-06-29 21:59:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import logging
 from pyrogram.handlers import *
 from plugin.comic_search import comicsearch
 from plugin.comic_index import comicindex
+from plugin.comic_index2 import comicindex2
 from plugin.comic_to_pdf import comic2Pdf
 from plugin.start import start
 
@@ -27,6 +28,7 @@ bot = Client(
 def main():
     bot.add_handler(MessageHandler(comicsearch, filters.regex(r'comic')), group=1)
     bot.add_handler(CallbackQueryHandler(comicindex, filters.regex('but_*')), group=2)
+    bot.add_handler(CallbackQueryHandler(comicindex2, filters.regex('but2_*')), group=3)
     bot.add_handler(CallbackQueryHandler(comic2Pdf, filters.regex('pdfr_*')), group=6)
     bot.add_handler(MessageHandler(start , filters.regex(r'start')), group=13)
 
