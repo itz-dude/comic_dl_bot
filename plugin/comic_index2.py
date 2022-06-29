afd899f09d70dcb8a47d25b5d483f30418541db8
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-06-29 21:58:07 +0530

    Create comic_index2.py

---
+++ plugin/comic_index2.py
@@ -0,0 +1,37 @@
+
+from pyrogram import *
+from pyrogram.types import *
+import requests
+from bs4 import BeautifulSoup
+
+def comicindex2(client, callback_query):
+    dataInitial = callback_query.data
+    dataSplit = dataInitial.split("_")
+    comicIndexNo = dataSplit[1]
+    comic = dataSplit[2]
+    comiclink = f"http://kissmanga.nl/manga/{mangaId}"
+    response = requests.get(comiclink)
+    plainText = response.text
+    source_url = BeautifulSoup(plainText, "html.parser")
+    title = souce_url.find('span', attrs={'class': 'title-1'}).text
+    chapters_div = source_url.find('tbody', attrs={'id': 'list'})
+    chapters = chapters_div.find_all('tr')
+    lastChapList = []
+    number = 1
+    for links in chapters:        
+        chapterNo = number
+        lastChapList.append(chapterNo)
+        number = number + 1
+    lastChapterNo = len(lastChapList)
+    listInitial = []
+    for i in range(0, int(lastChapterNo)):
+        listInitial.append(i)
+    n = 41
+    listOrganisedInitial = [listInitial[i:i + n] for i in range(0, len(listInitial), n)]
+    n = []
+    for kn in listOrganisedInitial[int(comicIndexNo)]:
+        n.append((InlineKeyboardButton(f'{kn}', callback_data=f"pdfr_{comic}_{kn}")))
+    m = 5
+    K = [n[i:i + m] for i in range(0, len(n), m)]
+    kb = InlineKeyboardMarkup(K)
+    callback_query.edit_message_reply_markup(reply_markup=kb)
